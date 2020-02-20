5a8d16560c79c493df240d26c51fbc08ec6bf37b
Christian Ponte <christian.ponte@udc.es>
2020-02-20 20:09:11 +0100

    Funcion lectura

--- test.py
+++ test.py
@@ -5,8 +5,22 @@ import re
 import numpy as np
 import pandas as pd
 
-file = "file"
+fname = "a_example.txt"
 
-with open(file) as f:
-    for l in f:
-        # read line
+
+def read_input(fin):
+    with open(fin) as f:
+        B, L, D = [int(v) for v in f.readline().split(' ')]
+        books = [int(v) for v in f.readline().split(' ')]
+        toggle = True
+        libraries = []
+        p = []
+        for l in f:
+            if toggle:
+                p.append([int(v) for v in l.split(' ')])
+            else:
+                p.append([int(v) for v in l.split(' ')])
+                libraries.append(p.copy())
+                p.clear()
+            toggle = not toggle
+    return B, L, D, books, libraries
