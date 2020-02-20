475df99431f72e63f2b4c8f9ca0b0da66ad36cfd
Marcos Horro <marcos.horro@udc.es>
2020-02-20 21:05:27 +0100

    Fixed output. Thanks God Markos for doing this

--- test.py
+++ test.py
@@ -5,7 +5,9 @@ import re
 import numpy as np
 import pandas as pd
 
-fname = "a_example.txt"
+fname = ["a_example.txt", "b_read_on.txt", "c_incunabula.txt",
+         "d_tough_choices.txt",
+         "e_so_many_books.txt", "f_libraries_of_the_world.txt"]
 
 
 def read_input(fin):
@@ -17,37 +19,52 @@ def read_input(fin):
         libraries = []
         p = []
         for l in f:
+            if l == "\n":
+                continue
             if toggle:
                 p.append([int(v) for v in l.split(' ')] + [idl])
             else:
-                p.append(sorted([int(v) for v in l.split(' ')], key=lambda i: books[i], reverse=True))
+                p.append(sorted([int(v) for v in l.split(' ')],
+                                key=lambda i: books[i], reverse=True))
                 libraries.append(p.copy())
                 p.clear()
                 idl = idl + 1
             toggle = not toggle
     return B, L, D, books, libraries
 
+
 def value(libraries, nbooks):
     v = sum([books[i] for i in libraries[1][0:nbooks]])
     print(v)
     return v
 
+
 def fitness(library, days):
     v = (value(library, days * library[0][2])/library[0][1])
     return v
 
+
 if __name__ == '__main__':
-    B, L, D, books, libraries = read_input(fname)
-    print(books)
-    libraries = sorted(libraries, key=lambda x: fitness(x, D))
-    print(libraries)
-    selected_libraries = []
-    while D > 0 and len(libraries) > 0:
-        D = D - libraries[-1][0][1]
-        if D < 0:
-            break
-        l = libraries[-1]
-        nbooks = D * l[0][2]
-        l[1] = l[1][0:nbooks]
-        selected_libraries.append(l)
-        libraries = libraries[0:-1]
+    for inputfile in fname:
+        B, L, D, books, libraries = read_input(inputfile)
+        print(books)
+        libraries = sorted(libraries, key=lambda x: fitness(x, D))
+        print(libraries)
+        selected_libraries = []
+        while D > 0 and len(libraries) > 0:
+            D = D - libraries[-1][0][1]
+            if D < 0:
+                break
+            l = libraries[-1]
+            nbooks = D * l[0][2]
+            l[1] = l[1][0:nbooks]
+            selected_libraries.append(l)
+            libraries = libraries[0:-1]
+
+        with open(inputfile.split(".txt")[0] + ".out", "w") as f:
+            f.write(str(len(selected_libraries)) + "\n")
+            for library in selected_libraries:
+                f.write(str(library[0][-1]) + " " + str(len(library[1])) + "\n")
+                for book in library[1]:
+                    f.write(str(book) + " ")
+                f.write("\n")
